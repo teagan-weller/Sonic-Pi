422ea7d4820ee90bfa38727ca764154bc1ba1c58
teagan-weller <99356297+teagan-weller@users.noreply.github.com>
2022-02-14 11:39:27 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,34 @@
 #sonic pi
+use_bpm 160
+use_synth :saw
+  
+  define :vee do
+    play :c4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :g4
+    sleep 0.5
+  end
+
+with_fx :bitcrusher do
+  vee
+end
+with_fx :whammy do
+  vee
+end
+
+with_fx :slicer do
+  vee
+end
+with_fx :wobble do
+  vee
+end
