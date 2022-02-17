381e962c3c077b1198044a903a5b895aa85cb364
teagan-weller <99356297+teagan-weller@users.noreply.github.com>
2022-02-17 12:11:49 -0800

    Update Senorita 2 Loop.rb

--- Senorita 2 Loop.rb
+++ Senorita 2 Loop.rb
@@ -1,87 +1,80 @@
 use_bpm 116
 
+define :cae do
+  play :c5
+  sleep 1
+  play :a4
+  sleep 1
+  play :e4
+  sleep 1
+  play :c5
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :g4
+  sleep 1
+  play :g4
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+end
+
+live_loop :crackle do
+  17.times do
+    sample :drum_cymbal_pedal
+    sleep 2
+  end
+  stop
+end
+
+sleep 7
+
 live_loop :treble do
-  play :c5
-  sleep 1
-  play :a4
-  sleep 1
-  play :e4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :g4
-  sleep 1
-  play :g4
-  sleep 0.5
-  play :c5
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :c5
-  sleep 1
-  play :a4
-  sleep 1
-  play :e4
-  sleep 1
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :g4
-  sleep 1
-  play :g4
-  sleep 0.5
-  play :c5
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :a4
-  sleep 0.5
-  play :g4
-  sleep 0.5
-  play :f4
-  sleep 0.75
-  play :c5
-  sleep 0.75
-  play :c5
-  sleep 1.5
-  play :f4
-  sleep 0.5
-  play :f4
-  sleep 0.75
-  play :c5
-  sleep 0.75
-  play :c5
-  sleep 0.5
-  play :c5
-  sleep 1.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :c5
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :g4
-  sleep 1
-  sleep 0.25
-  
-stop end
+  with_fx :vowel do
+    cae
+    cae
+    play :f4
+    sleep 0.75
+    play :c5
+    sleep 0.75
+    play :c5
+    sleep 1.5
+    play :f4
+    sleep 0.5
+    play :f4
+    sleep 0.75
+    play :c5
+    sleep 0.75
+    play :c5
+    sleep 0.5
+    play :c5
+    sleep 1.5
+    play :b4
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :g4
+    sleep 1
+    sleep 0.25
+    stop
+  end
+end
 
 live_loop :bass do
   play :a2
