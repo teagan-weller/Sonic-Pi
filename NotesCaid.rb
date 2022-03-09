38a26bb3ecaa78a01baa2119c41eb570bcabc68d
teagan-weller <99356297+teagan-weller@users.noreply.github.com>
2022-03-09 10:05:00 -0800

    Update and rename gayNotesCaid.rb to NotesCaid.rb

--- NotesCaid.rb
+++ NotesCaid.rb
@@ -1,12 +1,12 @@
 use_bpm 160
 use_synth :saw
-gayNotes = ["c2", "e2", "g2","b2", "c3", "b2", "g2", "e2"]
+Notes = ["c2", "e2", "g2","b2", "c3", "b2", "g2", "e2"]
 index = 0
 se = 7
 
 live_loop :main_theme do
   with_fx :distortion do
-    play (gayNotes[index])
+    play (Notes[index])
     sleep 0.5
     if (index < se)
       index = index + 1
